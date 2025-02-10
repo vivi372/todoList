952fe9022fe8d6ffeae861ddd795a770e53c1d30
ym970 <ym970@192.168.10.12>
2025-02-10 19:41:59 +0900

    사이트 매쉬 적용

--- todoList/src/main/java/com/todo/TodoListApplication.java
+++ todoList/src/main/java/com/todo/TodoListApplication.java
@@ -2,6 +2,10 @@ package com.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.todo.util.config.SiteMeshConfig;
 
 @SpringBootApplication
 public class TodoListApplication {
@@ -9,5 +13,16 @@ public class TodoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
+	
+	//사이트매쉬 빈 추가
+	@Bean
+	public FilterRegistrationBean<SiteMeshConfig> siteMeshFilter() {
+
+		FilterRegistrationBean<SiteMeshConfig> filter = new FilterRegistrationBean<SiteMeshConfig>();
+		filter.setFilter(new SiteMeshConfig());
+
+		return filter;
+
+	} 
 
 }
