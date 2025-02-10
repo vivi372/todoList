9511fe93966e4f9781d87e3c167381ebd0ce9fa9
ym970 <ym970@192.168.10.12>
2025-02-10 22:25:13 +0900

    리액트 사용을 위해 사이트매쉬 제거

--- todoList/src/main/java/com/todo/TodoListApplication.java
+++ todoList/src/main/java/com/todo/TodoListApplication.java
@@ -2,10 +2,7 @@ package com.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
-import com.todo.util.config.SiteMeshConfig;
 
 @SpringBootApplication
 public class TodoListApplication {
@@ -13,16 +10,5 @@ public class TodoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
-	
-	//사이트매쉬 빈 추가
-	@Bean
-	public FilterRegistrationBean<SiteMeshConfig> siteMeshFilter() {
-
-		FilterRegistrationBean<SiteMeshConfig> filter = new FilterRegistrationBean<SiteMeshConfig>();
-		filter.setFilter(new SiteMeshConfig());
-
-		return filter;
-
-	} 
 
 }
