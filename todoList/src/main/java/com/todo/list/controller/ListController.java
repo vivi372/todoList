e1e799cd5b5c0e18c0d9495823f43d2d5832e0bf
venivivi-admin <sg970825@gmail.com>
2025-02-23 21:25:28 +0900

    투두 리스트 백엔드 커밋

--- todoList/src/main/java/com/todo/list/controller/ListController.java
+++ todoList/src/main/java/com/todo/list/controller/ListController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.todo.list.model.ListDTO;
 import com.todo.list.service.ListService;
-import com.todo.member.model.MemberDTO;
-import com.todo.member.service.MemberService;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -41,6 +37,30 @@ public class ListController {
 		log.info(dto.toString());		
 		
 		return new ResponseEntity<>(service.todoWrite(dto),HttpStatus.OK);		
+		
+	}
+	
+	@PostMapping("/comple")
+	public ResponseEntity<Integer> comple(@RequestBody ListDTO dto) {
+		log.info(dto.toString());		
+		
+		return new ResponseEntity<>(service.todoComple(dto.getImpor(),dto.getId()),HttpStatus.OK);		
+		
+	}
+	
+	@PostMapping("/update")
+	public ResponseEntity<Integer> update(@RequestBody ListDTO dto) {
+		log.info(dto.toString());		
+		
+		return new ResponseEntity<>(service.todoUpdate(dto),HttpStatus.OK);		
+		
+	}
+	
+	@PostMapping("/delete")
+	public ResponseEntity<Integer> delete(@RequestBody ListDTO dto) {
+		log.info(dto.toString());		
+		
+		return new ResponseEntity<>(service.todoDelete(dto),HttpStatus.OK);		
 		
 	}	
 	
